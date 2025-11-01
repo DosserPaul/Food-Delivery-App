9016a64ca223696f7f4548d09725b181e99ae55a
Paul DOSSER <pauldosser@Mac.lan>
2025-11-01 13:54:56 +0100

    feat: logic cart

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,21 +1,23 @@
-import {View, Text, TouchableOpacity, Image} from 'react-native'
+import {Image, Text, TouchableOpacity, View} from 'react-native'
 import React from 'react'
 import {images} from "@/constants";
+import {useCartStore} from "@/store/cart.store";
 import {router} from "expo-router";
 
 const CartButton = () => {
-  const totalItems = Math.ceil(Math.random() * 100);
+  const {getTotalItems} = useCartStore();
+  const totalItems = getTotalItems();
 
-    return (
-        <TouchableOpacity className="cart-btn" onPress={()=> router.push('/cart')}>
-            <Image source={images.bag} className="size-5" resizeMode="contain" />
+  return (
+    <TouchableOpacity className="cart-btn" onPress={() => router.push('/cart')}>
+      <Image source={images.bag} className="size-5" resizeMode="contain"/>
 
-            {totalItems > 0 && (
-                <View className="cart-badge">
-                    <Text className="small-bold text-white">{totalItems}</Text>
-                </View>
-            )}
-        </TouchableOpacity>
-    )
+      {totalItems > 0 && (
+        <View className="cart-badge">
+          <Text className="small-bold text-white">{totalItems}</Text>
+        </View>
+      )}
+    </TouchableOpacity>
+  )
 }
 export default CartButton
--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,7 +1,9 @@
 import {Image, Platform, Text, TouchableOpacity} from 'react-native'
 import {MenuItem} from "@/type";
+import {useCartStore} from "@/store/cart.store";
 
 const MenuCard = ({item: {$id, image_url, name, price}}: { item: MenuItem }) => {
+  const {addItem} = useCartStore();
 
   return (
     <TouchableOpacity className="menu-card"
@@ -9,8 +11,7 @@ const MenuCard = ({item: {$id, image_url, name, price}}: { item: MenuItem }) => 
       <Image source={{uri: image_url}} className="size-32 absolute -top-10" resizeMode="contain"/>
       <Text className="text-center base-bold text-dark-100 mb-2" numberOfLines={1}>{name}</Text>
       <Text className="body-regular text-gray-200 mb-4">From ${price}</Text>
-      <TouchableOpacity onPress={() => {
-      }}>
+      <TouchableOpacity onPress={() => addItem({id: $id, name, price, image_url: image_url, customizations: []})}>
         <Text className="paragraph-bold text-primary">Add to Cart +</Text>
       </TouchableOpacity>
     </TouchableOpacity>
