974acd5d5d8178ed4e5262907649e3f00ea093f7
Paul DOSSER <pauldosser@Mac.lan>
2025-11-01 11:51:16 +0100

    feat: init search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,22 +1,68 @@
-import {Button, Text, View} from "react-native";
+import {FlatList, Text, View} from 'react-native'
 import {SafeAreaView} from "react-native-safe-area-context";
-import seed from "@/lib/seed";
+import useAppwrite from "@/lib/useAppwrite";
+import {getCategories, getMenu} from "@/lib/appwrite";
+import {useLocalSearchParams} from "expo-router";
+import {useEffect} from "react";
+import CartButton from "@/components/CartButton";
+import cn from "clsx";
+import MenuCard from "@/components/MenuCard";
+import {MenuItem} from "@/type";
+
 
 const Search = () => {
-  return (
-    <SafeAreaView>
-      <Text>Search</Text>
+    const { category, query } = useLocalSearchParams<{query: string; category: string}>()
 
-      <Button title="Seed Data" onPress={async () => {
-        try {
-          await seed();
-          console.log("Database seeded successfully.");
-        } catch (error) {
-          console.error("Error seeding database:", error);
-        }
-      }} />
-    </SafeAreaView>
-  )
+    const { data, refetch, loading } = useAppwrite({ fn: getMenu, params: { category,  query,  limit: 6, } });
+    const { data: categories } = useAppwrite({ fn: getCategories });
+
+    useEffect(() => {
+        refetch({ category, query, limit: 6})
+    }, [category, query]);
+
+    return (
+        <SafeAreaView className="bg-white h-full">
+            <FlatList
+                data={[]}
+                renderItem={({ item, index }) => {
+                    const isFirstRightColItem = index % 2 === 0;
+
+                    return (
+                        <View className={cn("flex-1 max-w-[48%]", !isFirstRightColItem ? 'mt-10': 'mt-0')}>
+                            <Text>{item.name}</Text>
+                        </View>
+                    )
+                }}
+                keyExtractor={item => item.$id}
+                numColumns={2}
+                columnWrapperClassName="gap-7"
+                contentContainerClassName="gap-7 px-5 pb-32"
+                ListHeaderComponent={() => (
+                    <View className="my-5 gap-5">
+                        <View className="flex-between flex-row w-full">
+                            <View className="flex-start">
+                                <Text className="small-bold uppercase text-primary">Search</Text>
+                                <View className="flex-start flex-row gap-x-1 mt-0.5">
+                                    <Text className="paragraph-semibold text-dark-100">Find your favorite food</Text>
+                                </View>
+                            </View>
+
+                            <CartButton />
+                        </View>
+
+                       {/*TODO: Add search bar*/}
+
+                        {/*TODO: Add filter options*/}
+                    </View>
+                )}
+                ListEmptyComponent={() => !loading && (
+                    <Text className="paragraph text-center text-dark-100 mt-10">
+                        No results found.
+                    </Text>
+                )}
+            />
+        </SafeAreaView>
+    )
 }
 
-export default Search;
+export default Search
